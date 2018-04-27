10cc172e6cf528ea63196e0377eee7a5560257ae
heshengchao <heshengchao@gome.com.cn>
2018-04-27 15:35:26 +0800

    fix bugs

--- peek-core/src/test/java/org/peek/protocol/MinaServerTest.java
+++ peek-core/src/test/java/org/peek/protocol/MinaServerTest.java
@@ -1,12 +1,13 @@
-package org.peek.collect.protocol;
+package org.peek.protocol;
 
-import org.junit.Test;
+import java.io.IOException;
+
 import org.peek.protocol.MinaServer;
 
 public class MinaServerTest {
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		MinaServer server=new MinaServer();
 		server.start();
 	}
