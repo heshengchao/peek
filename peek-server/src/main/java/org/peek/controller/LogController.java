cf738335d58b903000e4ed39e9b282c0c34c4bc9
heshengchao <heshengchao@gome.com.cn>
2018-05-14 10:40:33 +0800

    日志统计

--- peek-server/src/main/java/org/peek/controller/LogController.java
+++ peek-server/src/main/java/org/peek/controller/LogController.java
@@ -1,27 +1,38 @@
 package org.peek.controller;
 
 import java.io.IOException;
+import java.util.List;
 
-import org.peek.domain.AppGroup;
+import org.peek.domain.LoggerInfo;
 import org.peek.service.impl.AppService;
-import org.peek.utils.WebResult;
+import org.peek.service.impl.LoggerCountService;
+import org.peek.service.query.LoggerInfoQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/app")
 public class LogController {
 
+	@Autowired LoggerCountService loggerCountService;
 	@Autowired AppService appService;
 	
 	@RequestMapping(value = "/")
-	public ModelAndView  index() throws IOException{
+	public ModelAndView  index(@RequestParam(required=false) String groupId,
+			@RequestParam(required=false)String appInsId) throws IOException{
 		ModelAndView mv=new ModelAndView("logger");
+		LoggerInfoQuery query=new LoggerInfoQuery();
+		if(!StringUtils.isEmpty(groupId))
+			query.setAppGroupId(groupId);
+		if(!StringUtils.isEmpty(appInsId))
+			query.setAppInsId(appInsId);
 		
-		appService.addGroup(group);
-		
-		return WebResult.ok();
+		List<LoggerInfo> list=loggerCountService.findByApp(query);
+		mv.addObject("loggerList", list);
+		return mv;
 	}
 }
