bbf130cc3bc5da9cc1fc2cb6b01f3e5d213887f2
heshengchao <heshengchao@gome.com.cn>
2018-06-30 16:49:00 +0800

    fix

--- peek-server/src/main/java/org/peek/controller/IndexController.java
+++ peek-server/src/main/java/org/peek/controller/IndexController.java
@@ -2,6 +2,7 @@ package org.peek.controller;
 
 import java.io.IOException;
 import org.peek.service.impl.AppService;
+import org.peek.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +11,13 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class IndexController {
 	@Autowired AppService appService;
+	@Autowired UserService userService;
 	
 	@RequestMapping(value = "/")
 	public ModelAndView  index() throws IOException{
 		ModelAndView mv=new ModelAndView("index");
 		mv.addObject("appInsList", appService.listAppIns()) ;
+		mv.addObject("userList", userService.findAll()) ;
 		return mv;
 	}
 }
