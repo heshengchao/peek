2ffe11b671f7e738472ee3fa94ba9de9b5867994
heshengchao <heshengchao@gome.com.cn>
2018-07-04 16:10:17 +0800

    日志排序

--- peek-server/src/main/java/org/peek/repository/LoggerCountRepository.java
+++ peek-server/src/main/java/org/peek/repository/LoggerCountRepository.java
@@ -44,7 +44,7 @@ public class LoggerCountRepository  {
 		if(query.getEndTime()!=null){
 			criteria.and("time").lte(query.getEndTime());
 		}
-		querys.with(Sort.by(Sort.Direction.DESC, "time"));  
+		querys.with(Sort.by(Sort.Direction.ASC, "time"));  
 		querys.limit(topn);
 		return mongoTemplate.find(Query.query(criteria),LoggerInfo.class);
 	}
